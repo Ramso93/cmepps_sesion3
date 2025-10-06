35ac49740d3b5ed9dc13727befe079cb3a83d549
Ramso93 <villcachoqueosmar@gmail.com>
2025-10-06 18:29:29 +0200

    TDD 2do paso - Green

---
+++ src/pkg/Calculadora.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Calculadora {
+
+	public static int suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	public static int resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	public static int multiplica(int i, int j) {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+	public static int divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+}
